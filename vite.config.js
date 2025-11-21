b30ecd6bc06ae99269279ffcee76cc75b28b028c
azath4u <azath4u@gmail.com>
2025-11-21 20:56:10 +0530

    rr

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  
+  base: '/tailwindvite/',   // 👈 VERY IMPORTANT
   plugins: [vue(), tailwindcss()],
 })
